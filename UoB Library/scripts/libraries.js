db932f087a39b5916b2d51b615a04096d7628c02
Rich <richmbrown@gmail.com>
2014-06-06 15:28:38 +0100

    add in handlebars and map

--- UoB Library/scripts/libraries.js
+++ UoB Library/scripts/libraries.js
@@ -1,6 +1,7 @@
 var librariesList;
 
 $(function() {
+	google.maps.event.addDomListener(window, "load", initMap);
 
 	$.getJSON("data/libraries.json", function(data){
 		var libraryList = $('#libraryList');
@@ -20,12 +21,24 @@ $(function() {
 		}
 	});
 
+	$('#library-record').on("pageshow", function( event ) {
+		google.maps.event.trigger(map, 'resize');
+	} )
+
+	Handlebars.registerHelper('formattedAddress', function(address) {
+		return address.replace(/,+/g, "<br />");
+	});
+
 });
+
+function sayHello(){
+	return "hello";
+}
 
 function injectHtml(data, templateFile, element, success){
 	$.get(templateFile, function(tmpl) {
-		var html = Mustache.to_html(tmpl, data);
-		element.html(html);
+		var template = Handlebars.compile(tmpl);
+		element.html(template(data));
 		if(success !== undefined){
 			success();
 		}
@@ -46,11 +59,17 @@ function showLibrary( urlObj, options )
 			// Get the content area element for the page.
 			$content = $page.children( ":jqmData(role=content)" );
 
-	var library = jQuery.grep(librariesList.libraries, function(item, i){return item.id == urlObj.hash.replace('#library-record?id=','')});
+	var library = jQuery.grep(librariesList.libraries, function(item, i){
+		return item.id == urlObj.hash.replace('#library-record?id=','');
+	})[0];
 
-	injectHtml(library[0], 'templates/library_record.tmpl', $content);
-
+	$header.children('h1').text(library.name)	
+		injectHtml(library, 'templates/library_record.tmpl', $content.children('#library-details'));
+	if((library.lat !== "" && library.lat !== undefined) && (library.long !== "" && library.long !== undefined)){
+		map.setCenter(new google.maps.LatLng(library.lat, library.long) );
+	}
 	$page.page();
+
 
 	// We don't want the data-url of the page we just modified
 	// to be the url that shows up in the browser's location field,
@@ -58,7 +77,19 @@ function showLibrary( urlObj, options )
 	// we just loaded.
 	options.dataUrl = urlObj.href;
 
+
 	// Now call changePage() and tell it to switch to
 	// the page we just modified.
 	$.mobile.changePage( $page, options );
+
+}
+
+function initMap(){
+	var mapOptions = {
+		zoom: 18,
+		center: new google.maps.LatLng(52.450668,-1.930452)
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
+
 }
