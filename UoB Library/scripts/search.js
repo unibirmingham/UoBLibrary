32ed88ca4a02d046f05024d8d5ca7aa80f9e499d
Rich <richmbrown@gmail.com>
2014-06-19 09:42:40 +0100

    begin search fetch

--- UoB Library/scripts/search.js
+++ UoB Library/scripts/search.js
@@ -1,6 +1,14 @@
 $(function() {
 	route('search-record', showSearchRecord);
 
+	$('#search-button').click(function(){
+		$.get('http://findit-s.bham.ac.uk:1701/PrimoWebServices/xservice/search/brief?institution=44BIR&onCampus=false&query=any,contains,english&indx=1&bulkSize=2&dym=true&highlight=true&lang=eng',
+			function(xml){ 
+				var json = $.xml2json(xml);
+
+			}); 
+	});
+
 });
 
 function showSearchRecord(){
